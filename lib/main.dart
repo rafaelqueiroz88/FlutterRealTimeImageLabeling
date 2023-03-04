f233fe30aedb83ed9bb1bf694a63bfcf4576d393
Rafael Queiroz de Castro <rafael.castro6@fatec.sp.gov.br>
2023-03-04 15:08:07 -0300

    First Commit
    Project Starting

---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
+
+late List<CameraDescription> _cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  _cameras = await availableCameras();
+  runApp(const CameraApp());
+}
+
+class CameraApp extends StatefulWidget {
+  const CameraApp({Key? key}) : super(key: key);
+
+  @override
+  State<CameraApp> createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraApp> {
+  String result = 'Your Image will be labelled here';
+  late CameraController cameraController;
+  CameraImage? img;
+  bool isBusy = false;
+  dynamic imageLabeler;
+
+  @override
+  void initState() {
+    super.initState();
+    final ImageLabelerOptions options =
+        ImageLabelerOptions(confidenceThreshold: 0.5);
+    imageLabeler = ImageLabeler(options: options);
+    startCamera();
+  }
+
+  void startCamera() async {
+    cameraController = CameraController(_cameras[0], ResolutionPreset.high,
+        enableAudio: false);
+
+    await cameraController.initialize().then((_) {
+      if (!mounted) return;
+
+      cameraController.startImageStream((image) {
+        if (!isBusy) {
+          isBusy = true;
+          setState(() {
+            img = image;
+          });
+          doImageLabeling();
+        }
+      });
+    }).catchError((err) {
+      print(err);
+    });
+  }
+
+  @override
+  void dispose() {
+    cameraController.dispose();
+    super.dispose();
+  }
+
+  doImageLabeling() async {
+    result = '';
+    InputImage inputImg = getInputImage();
+    final List<ImageLabel> labels = await imageLabeler.processImage(inputImg);
+    for (ImageLabel label in labels) {
+      final String text = label.label;
+      final int index = label.index;
+      final double confidence = label.confidence;
+      result += "$text   ${confidence.toStringAsFixed(2)}\n";
+    }
+    setState(() {
+      result;
+      isBusy = false;
+    });
+  }
+
+  InputImage getInputImage() {
+    final WriteBuffer allBytes = WriteBuffer();
+    for (final Plane plane in img!.planes) {
+      allBytes.putUint8List(plane.bytes);
+    }
+    final bytes = allBytes.done().buffer.asUint8List();
+
+    final Size imageSize = Size(img!.width.toDouble(), img!.height.toDouble());
+
+    final camera = _cameras[0];
+    final imageRotation =
+        InputImageRotationValue.fromRawValue(camera.sensorOrientation);
+    // if (imageRotation == null) return;
+
+    final inputImageFormat =
+        InputImageFormatValue.fromRawValue(img!.format.raw);
+    // if (inputImageFormat == null) return null;
+
+    final planeData = img!.planes.map(
+      (Plane plane) {
+        return InputImagePlaneMetadata(
+          bytesPerRow: plane.bytesPerRow,
+          height: plane.height,
+          width: plane.width,
+        );
+      },
+    ).toList();
+
+    final inputImageData = InputImageData(
+      size: imageSize,
+      imageRotation: imageRotation!,
+      inputImageFormat: inputImageFormat!,
+      planeData: planeData,
+    );
+
+    final inputImage =
+        InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
+
+    return inputImage;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!cameraController.value.isInitialized) {
+      return Container();
+    }
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: [
+            CameraPreview(cameraController),
+            Center(
+              child: Text(
+                result,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
